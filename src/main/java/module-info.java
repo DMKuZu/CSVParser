c7ec4aab88bc3eb994c282d0c5aa8e42deea604e
JwLappie <john.winston.tabada@gmail.com>
2025-03-23 01:45:32 +0800

    john

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.kuzu.csvparser {
     requires javafx.controls;
     requires javafx.fxml;
     requires commons.csv;
-    requires org.apache.pdfbox;
     requires java.desktop;
     requires openhtmltopdf.pdfbox;
     requires openhtmltopdf.core;
